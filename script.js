ae53ae3c36e39ce2fa0ef5df6a2a82fbb57fda3b
AJ <anmolsingh51200@gmail.com>
2024-05-04 14:45:19 +0530

    edit darkmode

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-
+//diclaration
 const button =document.getElementsByClassName('num');
-const clear = document.getElementById ('C');
+const clear = document.getElementById('C');
 let display = document.getElementById('display');
 const temp = document.getElementById('=');
+const body= document.querySelector("body");
+const toggle = document.getElementById('check');
+const toggleBox =document.getElementById('toggle')
+let bool =true;
+
+
+//function for toggle
+toggle.addEventListener("click",()=>{
+    if(bool==false){
+        bool=true;
+        body.style.backgroundColor="rgba(0, 0, 0, 0.749)";
+        toggleBox.style.backgroundColor="black";
+        
+    }
+    else{
+        body.style.backgroundColor="beige";
+        toggleBox.style.backgroundColor="lightgray";
+        bool=false;
+        
+    }
+})
+
 temp.addEventListener("click",sum);
 let element = "";
+
+
+
+
 //function for numaric button 
-
-
 function mydoc(el){
     
     display.value += el.srcElement.innerHTML;
     
 }
-console.log();
+
 //function for symbol button
 function print(el){
     display.value += el.srcElement.innerHTML;
     element = el.target;
     
 }
-
 
 //fuction for equal button
 function sum(){
@@ -31,7 +54,7 @@ function sum(){
 for(let i=0; i<button.length;i++){
 
     button[i].addEventListener("click",print);
-
+    button[i].addEventListener("touch",print);
 
 }
 
